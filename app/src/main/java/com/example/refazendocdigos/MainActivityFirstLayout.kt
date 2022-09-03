1b9f5a35b4867fd343d7dbeff1ab7e3e1815a359
erickferrareto <e.ferraretowork@gmail.com>
2022-09-03 16:02:12 -0300

    First commit - Rt

---
+++ app/src/main/java/com/example/refazendocdigos/MainActivityFirstLayout.kt
@@ -0,0 +1,11 @@
+package com.example.refazendocdigos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityFirstLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_first_layout)
+    }
+}
